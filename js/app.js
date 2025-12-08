05427bd72e0c8a0c09cf09a1085cf2eeb809e6fc
Snayder Polonia <snayderpolonia2005@gmail.com>
2025-12-07 23:16:26 -0400

    Activar y desactivar boton de enviar
    Si no hay campos vacíos activa el botón de enviar, de lo contrario desactivado.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
 
+    const campos = {
+        email: '',
+        asunto: '', 
+        mensaje: ''
+    }
+
     // Seleccionar los elementos de la Interfaz.
 
     const inputEmail = document.querySelector('#email');
     const inputAsunto = document.querySelector('#asunto');
     const inputMensaje = document.querySelector('#mensaje');
     const formulario = document.querySelector('#formulario');
+    const botom = document.querySelector('#formulario button[type="submit"]');
 
     // Asingar Eventos
 
@@ -19,15 +26,25 @@ document.addEventListener('DOMContentLoaded', function() {
         // console.log(e.target.parentElement.nextElementSibling);
         if (e.target.value.trim() === '') {
             mostrarAlerta(`El campo ${e.target.id} es obligatorio`, e.target.parentElement);
+            campos[e.target.name] = ''; // Reinicia valor en objeto. 
+            comprobarCampos();
             return; 
         }
         // Llamar validarEmail, mostrar error si es false, solo para campo email. 
         if (e.target.id === 'email' && !validarEmail(e.target.value)) {
             mostrarAlerta('El email no es valido', e.target.parentElement);
+            campos[e.target.name] = ''; // Reinicia valor en objeto. 
+            comprobarCampos();
             return;
         }
         
         eliminarAlerta(e.target.parentElement);
+
+        // Asigar los valores
+        campos[e.target.name] = e.target.value.trim().toLowerCase();
+
+        // Comprobar el objeto de campos. 
+        comprobarCampos();
     }
 
     function mostrarAlerta(mensaje, referencia) {
@@ -56,6 +73,17 @@ document.addEventListener('DOMContentLoaded', function() {
         const regex =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/
         const resultado = regex.test(email);
         return resultado;
+    }
+
+    // Habilitar o Desactivar botom de enviar. 
+    function comprobarCampos() {
+        if (Object.values(campos).includes('')) { // Si tiene valores vacios. 
+            botom.disabled = true; 
+            botom.classList.add('opacity-50');
+        } else { // Si no tiene. 
+            botom.classList.remove('opacity-50');
+            botom.disabled = false; 
+        }
     }
  
 });
